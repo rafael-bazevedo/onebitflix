4320689f9457d2fdb68bfda964c1fa51d08bf0e9
Rafael Azevedo <rafael.bimbatto.azevedo@gmail.com>
2024-08-01 21:47:09 -0300

    Obtendo informações do curso

--- src/models/index.ts
+++ src/models/index.ts
@@ -3,8 +3,8 @@ import { Course } from "./Course";
 import { Episode } from "./Episode";
 import { User } from "./User";
 
-Category.hasMany(Course)
 Category.hasMany(Course, { as: 'courses' })
+Course.hasMany(Episode, { as: 'episodes' })
 
 Course.belongsTo(Category)
 Course.hasMany(Episode)
