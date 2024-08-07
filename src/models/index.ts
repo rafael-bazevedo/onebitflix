15959e111b8c4e1a5b3a332233faf784d1df4204
Rafael Azevedo <rafael.bimbatto.azevedo@gmail.com>
2024-08-06 23:45:36 -0300

    Criando o gostei

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,12 +2,14 @@ import { Category } from "./Category";
 import { Course } from "./Course";
 import { Episode } from "./Episode";
 import { Favorite } from "./Favorite";
+import { Like } from "./Like";
 import { User } from "./User";
 
 Category.hasMany(Course, { as: 'courses' })
 
 Course.belongsTo(Category)
 Course.belongsToMany(User, { through: Favorite })
+Course.belongsToMany(User, { through: Like })
 Course.hasMany(Episode, { as: 'episodes' })
 Course.hasMany(Favorite, { as: 'favoritesUsers', foreignKey: 'course_id' })
 
@@ -17,11 +19,14 @@ Favorite.belongsTo(Course)
 Favorite.belongsTo(User)
 
 User.belongsToMany(Course, { through: Favorite })
+User.belongsToMany(Course, { through: Like })
 User.hasMany(Favorite, { as: 'favoritesCourses', foreignKey: 'user_id' })
 
 export {
   Course,
   Category,
   Episode,
+  Favorite,
+  Like,
   User
 }
