1e03a8f61efdbc5f2b3ccbcdf35a2fcfbba43c61
Rafael Azevedo <rafael.bimbatto.azevedo@gmail.com>
2024-08-26 09:41:39 -0300

    Atualizando o endpoint de cursos

--- src/services/favoriteService.ts
+++ src/services/favoriteService.ts
@@ -38,5 +38,16 @@ export const favoriteService = {
         courseId
       }
     })
+  },
+
+  isFavorited: async (userId: Number, courseId: Number) => {
+    const favorite = await Favorite.findOne({
+      where: {
+        userId,
+        courseId
+      }
+    })
+
+    return favorite !== null
   }
 }
