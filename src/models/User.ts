a9f1e9da4d672aecf731c41dd0e104d53542ee54
Rafael Azevedo <rafael.bimbatto.azevedo@gmail.com>
2024-08-26 14:28:30 -0300

    Obtendo a lista de continuar assistindo

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,6 +1,7 @@
 import { DataTypes, Model, Optional } from "sequelize"
 import { database } from "../database"
 import bcrypt from 'bcrypt'
+import { EpisodeInstance } from "./Episode"
 
 type CheckPasswordCallback = (err?: Error, isSame?: boolean) => void
 
@@ -19,6 +20,7 @@ export interface UserCreationAttributes
   extends Optional<User, 'id'> {}
 
 export interface UserInstance extends Model<User, UserCreationAttributes>, User {
+  Episodes?: EpisodeInstance[]
   checkPassword: (password: string, callbackfn: CheckPasswordCallback) => void
 }
 
